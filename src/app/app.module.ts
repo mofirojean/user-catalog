ba86fe854735d35960aa3fd5514d3c92e1ea6fc4
mofirojean <mofirojean@gmail.com>
2023-03-04 15:38:07 +0100

    feat: added main modules and http for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './component/users/users.component';
+import { UserDetailComponent } from './component/user-detail/user-detail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
