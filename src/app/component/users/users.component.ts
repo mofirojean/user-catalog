a5ef58a769d277a16ecf9dad79f8b9bbe657df46
mofirojean <mofirojean@gmail.com>
2023-03-04 15:37:03 +0100

    feat: created and implemented the user comp

---
+++ src/app/component/users/users.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Response } from 'src/app/interface/response';
+import { UserService } from 'src/app/service/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  response?: Response;
+
+  constructor(
+    private userService: UserService
+  ) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers(15).subscribe(
+      (results: Response) => {
+        console.log(results)
+        this.response = results
+      }
+    )
+  }
+
+}
