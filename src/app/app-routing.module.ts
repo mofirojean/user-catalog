15c0bb9f4775858d4c9fa0fa04853663707bf299
mofirojean <mofirojean@gmail.com>
2023-03-04 15:35:28 +0100

    feat: created the routes for the various component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './component/users/users.component';
+import { UserDetailComponent } from './component/user-detail/user-detail.component';
+import { UserResolver } from './service/resolver/user.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "users", component: UsersComponent, title: "users"},
+  {path: "user/:uuid", component: UserDetailComponent, title: "user", resolve: {resolvedResponse: UserResolver}},
+  {path: "**", redirectTo: "users"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
